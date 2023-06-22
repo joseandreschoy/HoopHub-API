c64bb6fe25057564daeed0e4618e675b05018e10
Jose Andres Choy De Leon <joseandreschoy@gmail.com>
2023-06-22 16:54:43 +0100

    created auth in userscontroller

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,3 +1,36 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+
+// ...
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    return res.json({ token });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
 // Get all users
 exports.getUsers = (req, res) => {
   res.json(users);
